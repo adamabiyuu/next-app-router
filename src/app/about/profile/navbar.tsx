6f69c94efac69f18afc4f4d553faa51827295388
adamabiyuu <advmabiyu@gmail.com>
2026-08-26 16:05:30 +0700

    first commit

--- src/app/about/profile/navbar.tsx
+++ src/app/about/profile/navbar.tsx
@@ -1,8 +1,7 @@
-
-export default function Home() {
+export default function Navbar() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello dunia
+      Navbar
     </main>
   )
 }
--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,8 +1,9 @@
+import Navbar from "./navbar";
 
-export default function Home() {
+export default function ProfilePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello dunia
+      <Navbar />
     </main>
   )
 }
