388b55d01ae87df7a0207ca40fd5c59de68b9f07
adamabiyuu <advmabiyu@gmail.com>
2026-08-26 19:12:25 +0700

    feat: learning next js

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,16 @@
+export default function AboutLayout({children} : {children: React.ReactNode}) {
+    return (
+      <>
+        <nav className="fixed left-0 top-10 z-10 h-screen w-60  bg-gray-800 py-2 px-5">
+          <ul className="px-5 py-5 text-white">
+            <li>Home</li>
+            <li>About</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+        <div className="">
+            {children}
+        </div>
+      </>
+    );
+}
--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./navbar";
+import Navbar from "../../navbar";
 
 export default function ProfilePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Navbar />
+      <h1>Profile Page</h1>
     </main>
   )
 }
